242359d6bbe03e126a1a29b1fc5fd968252a4aaf
pchmielecki87 <pchmielecki87@gmail.com>
2021-03-20 19:55:19 +0100

    added required dependencies

--- App/requirements.r
+++ App/requirements.r
@@ -1,6 +1,9 @@
 # R app
 install.packages("devtools")
 devtools::install_github("jcheng5/googleCharts")
+install.packages("googleCharts")
+install.packages("shiny")
+install.packages("dplyr")
 
 # db_connection
 devtools::install_github("RcppCore/Rcpp")
